35a3d178d1749e9ec49158546083dd1c93634362
Unknown <cedriclingom@rocketmail.com>
2017-07-24 16:21:19 +0200

    initialisation

---
+++ liste_contigue/liste_contigue.h
@@ -0,0 +1,41 @@
+#ifndefine _BIBLIOTHEQUE_C_LISTE_CONTIGUE
+
+
+
+#define 1_MOT
+
+
+
+
+
+
+typedef int objet;
+
+
+
+
+#if 1_MOT
+
+
+#endif
+
+/*echange*/
+/*inverser*/
+/*afficher*/
+/*longueur de la liste*/
+/*initialiser liste à 0*/
+/*rechercher valeur dans une liste non trié*/
+/*rechercher valeur dans une liste non trié*/
+/*supprimer un élément*/
+/*décalage à gauche*/
+/*décalage à droite*/
+/*inserer un élément en minimisant le décalage*/
+/*Allocation et libération de mémoire*/
+/*insertion d'élément avant et après un élément d'adress a*/
+/*liste contigue pleine*/
+/*comparer une chaine de taile N*/
+/*Insertion d'une valeur entrer au clavier avant une valeur v dans la liste*/
+
+
+
+#endif
